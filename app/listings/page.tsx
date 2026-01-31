f4dd45519d96f31443a009e651101b0c3afcddbc
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-01-31 18:47:04 +0300

    Update page.tsx

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { supabase } from '../lib/supabase'
+// İki kat yukarı çıkıp lib'e giriyoruz
+import { supabase } from '../../lib/supabase'
 
 export default function Listings() {
   const [listings, setListings] = useState<any[]>([])
