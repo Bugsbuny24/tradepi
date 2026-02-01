b18fb05124a6422a77b086ee587378ae6565e996
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-01 12:17:45 +0300

    Create B2CStub.vue

---
+++ src/views/stubs/B2CStub.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p">
+    <h1>B2C Module</h1>
+    <p>B2C: katalog + ürün detayı. RFQ zorunlu değil. İletişim önde.</p>
+    <RouterLink to="/">← Home</RouterLink>
+  </div>
+</template>
+<style scoped>.p{padding:20px}</style>
