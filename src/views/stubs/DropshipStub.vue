12339d7e07c96472ca80ef572294790f233bde96
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-01 12:20:09 +0300

    Create DropshipStub.vue

---
+++ src/views/stubs/DropshipStub.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p">
+    <h1>Dropshipping / On-demand</h1>
+    <p>Supplier sync + fulfillment dışarıda. Biz vitrin + yönlendirme.</p>
+    <RouterLink to="/">← Home</RouterLink>
+  </div>
+</template>
+<style scoped>.p{padding:20px}</style>
