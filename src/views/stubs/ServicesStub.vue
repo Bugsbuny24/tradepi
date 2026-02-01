a94c0d803f2de1544252af5b802b2aa8ecc53591
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-01 12:18:44 +0300

    Create ServicesStub.vue

---
+++ src/views/stubs/ServicesStub.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p">
+    <h1>Services (Fiverr-style)</h1>
+    <p>Gig + paket + order akışı. RFQ burada “iş brief’i” gibi.</p>
+    <RouterLink to="/">← Home</RouterLink>
+  </div>
+</template>
+<style scoped>.p{padding:20px}</style>
