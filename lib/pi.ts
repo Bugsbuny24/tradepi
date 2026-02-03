06ea75213cbed0836b39b85e49a53616f6cdb40a
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-03 10:21:34 +0300

    Delete lib directory

--- lib/pi.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface Window {
-    Pi?: any;
-  }
-}
-
-export function ensurePiReady() {
-  return typeof window !== "undefined" && !!window.Pi;
-}
--- lib/piConfig.ts
+++
@@ -1,2 +0,0 @@
-export const PI_API_KEY = process.env.PI_API_KEY!;
-export const PI_PLATFORM_API = "https://api.minepi.com";
