9910c72fc0c96fd6f953fe78b90678b705d806e7
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-10 23:27:13 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="tr">
       <body>
-        <Script
-          src="https://sdk.minepi.com/pi-sdk.js"
-          strategy="beforeInteractive"
-        />
+        <Script src="https://sdk.minepi.com/pi-sdk.js" strategy="beforeInteractive" />
         {children}
       </body>
     </html>
