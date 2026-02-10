ddcbc7687bbccf02d887b2e6d7a5bf03451da98a
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-10 20:35:04 +0300

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-"use client";
 
+"use client";
+export const dynamic = "force-dynamic";
 import React, { useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
