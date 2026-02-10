8e72ff018f7da957744b21886cedf663fc37931c
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-10 22:58:32 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,13 @@ import type { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="tr">
-      <head>
-        {/* Pi SDK should be available ASAP (Pi Browser) */}
-        <Script src="https://sdk.minepi.com/pi-sdk.js" strategy="beforeInteractive" />
-      </head>
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://sdk.minepi.com/pi-sdk.js"
+          strategy="beforeInteractive"
+        />
+        {children}
+      </body>
     </html>
   );
 }
