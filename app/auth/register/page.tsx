4afbe8eeffd6db931be8b0a168d2a60f6bd1b757
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-11 10:12:41 +0300

    Delete app/auth directory

--- app/auth/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "./register-form";
-
-export default function RegisterPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center p-6">
-      <RegisterForm />
-    </div>
-  );
-}
