40811a16bd3f5de3b958e844f8fef237735e918a
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-11 07:35:34 +0300

    Create button.tsx

---
+++ components/ui/button.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export function Button({
+  className = "",
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      className={`px-4 py-2 rounded bg-black text-white font-bold ${className}`}
+      {...props}
+    />
+  );
+}
