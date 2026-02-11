c78386b4e838c9a0cb1c7f53da55c02129657c78
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-11 07:12:19 +0300

    Delete token.ts

--- token.ts
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-export function tokenPrefix(token: string) {
-  return token.slice(0, 10);
-}
-
-export function tokenHashSha256(token: string) {
-  return crypto.createHash("sha256").update(token).digest("hex");
-}
