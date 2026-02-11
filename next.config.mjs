5ddf65b9d3e0f84b33b78275ec658ec5c656dde3
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-11 13:57:06 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
+  // If you were using static export, disable it to avoid useSearchParams suspense errors.
+  // output: "export",
 };
 
 export default nextConfig;
