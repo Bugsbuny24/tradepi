b2593c798538c8bfb3a835a9d5ad0ef25e5de98f
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-11 07:40:46 +0300

    Create route.ts

---
+++ lib/supabase/route.ts
@@ -0,0 +1,6 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+  return createRouteHandlerClient({ cookies });
+}
