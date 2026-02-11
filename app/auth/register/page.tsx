0c58964671b6497517e9b8382de5a2abd2b29a70
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-11 07:27:39 +0300

    Create page.tsx

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "./register-form";
+
+export default function RegisterPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <RegisterForm />
+    </div>
+  );
+}
