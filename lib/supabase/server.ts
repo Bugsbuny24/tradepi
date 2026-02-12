fac7db385c46830059a0a4280bd00c7ec84711f5
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-12 08:06:35 +0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,14 +1,16 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export function createClient() {
+export function createServerSupabaseClient() {
+  const cookieStore = cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name) {
-          return cookies().get(name)?.value;
+          return cookieStore.get(name)?.value;
         },
       },
     }
