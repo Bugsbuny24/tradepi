34a72aab947217ab9aef84d154b9b15a2c14e4d2
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-12 08:25:12 +0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -9,7 +9,7 @@ export function createServerSupabaseClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return cookieStore.get(name)?.value;
         },
       },
