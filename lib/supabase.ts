806181a2f84c6dcb4852ed8fea1e5d35af59ba48
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-12 11:21:11 +0300

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import { createBrowserClient } from '@supabase/ssr'
+
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        flowType: 'pkce',
+        detectSessionInUrl: true,
+        persistSession: true, // Oturumu hafızada tut
+        storageKey: 'tradepi-auth-token' // Özel anahtar
+      }
+    }
+  )
