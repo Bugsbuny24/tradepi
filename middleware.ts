a4a5e832147787bca0a156f618f705165aee9326
Bugsbuny24 <99527234+Bugsbuny24@users.noreply.github.com>
2026-02-14 16:31:48 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -35,3 +35,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/dashboard/:path*', '/create/:path*'],
 }
+export const config = {
+  matcher: ['/dashboard/:path*', '/create/:path*', '/admin/:path*'], // Admin eklendi!
+}
